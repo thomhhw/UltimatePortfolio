bf1ec5de5c9bdd2034e4573ca3e6b1058a5b36e6
Thom Pheijffer <tpheijffer@icloud.com>
2020-10-22 17:18:59 +0200

    Initial Commit

---
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolioApp.swift
+//  UltimatePortfolio
+//
+//  Created by Thom Pheijffer on 22/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
