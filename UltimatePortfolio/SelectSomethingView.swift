c2af91f65865228421a7291e7b54219268264be7
Thom Pheijffer <tpheijffer@icloud.com>
2020-11-20 14:40:49 +0100

    Added award screen

---
+++ UltimatePortfolio/SelectSomethingView.swift
@@ -0,0 +1,22 @@
+//
+//  SelectSomethingView.swift
+//  UltimatePortfolio
+//
+//  Created by Thom Pheijffer on 20/11/2020.
+//
+
+import SwiftUI
+
+struct SelectSomethingView: View {
+    var body: some View {
+        Text("Please select something from the menu to begin.")
+            .italic()
+            .foregroundColor(.secondary)
+    }
+}
+
+struct SelectSomethingView_Previews: PreviewProvider {
+    static var previews: some View {
+        SelectSomethingView()
+    }
+}
