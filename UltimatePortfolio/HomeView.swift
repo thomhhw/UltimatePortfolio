a12c622c5bef7aab14a83a7d89491dba984eead3
Thom Pheijffer <tpheijffer@icloud.com>
2020-12-15 16:23:46 +0100

    Added accessibility

--- UltimatePortfolio/HomeView.swift
+++ UltimatePortfolio/HomeView.swift
@@ -54,6 +54,8 @@ struct HomeView: View {
                                 .background(Color.secondarySystemGroupedBackground)
                                 .cornerRadius(10)
                                 .shadow(color: Color.black.opacity(0.2), radius: 5)
+                                .accessibilityElement(children: .combine)
+                                .accessibilityLabel("\(project.projectTitle), \(project.projectItems.count) items, \(project.completionAmount * 100, specifier: "%g")% complete.")
                             }
                         }
                         .padding([.horizontal, .top])
