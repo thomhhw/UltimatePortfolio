d6ad35628f2e8cde2cc6bfbb01c46f054d831343
Thom Pheijffer <tpheijffer@icloud.com>
2021-04-01 19:16:46 +0200

    Performance test added

---
+++ UltimatePortfolioTests/PerformanceTests.swift
@@ -0,0 +1,26 @@
+//
+//  PerformanceTests.swift
+//  UltimatePortfolioTests
+//
+//  Created by Thom Pheijffer on 01/04/2021.
+//
+
+import XCTest
+@testable import UltimatePortfolio
+
+class PerformanceTests: BaseTestCase {
+    func testAwardCalculationPerformance() throws {
+        // Create a significant amount of test data
+        for _ in 1...100 {
+            try dataController.createSampleData()
+        }
+
+        // Simulate lots of awards to check
+        let awards = Array(repeating: Award.allAwards, count: 25).joined()
+        XCTAssertEqual(awards.count, 500, "This checks the awards count is constant. Change this if you add awards.")
+
+        measure {
+            _ = awards.filter(dataController.hasEarned)
+        }
+    }
+}
